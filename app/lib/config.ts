760806ce6e4baacf5dd5a62148ee04c039928f6b
gentle-wave <adebayooluwatobi2018@gmail.com>
2026-03-11 21:23:42 +0100

    fix

--- app/lib/config.ts
+++ app/lib/config.ts
@@ -1,1 +1,2 @@
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:4000';
+// export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL ?? 'http://localhost:4000';
+export const API_BASE_URL = 'https://38aa-102-93-14-196.ngrok-free.app'
